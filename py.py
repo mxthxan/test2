1b12f09741f4496f7c7b739a9a33678b2d3b2f65
mxthxan <165267866+mxthxan@users.noreply.github.com>
2024-07-05 18:30:05 +0530

    assignment8

---
+++ py.py
@@ -0,0 +1,11 @@
+# Getting the input from the user and converting it to an integer
+number = int(input("Enter a value: "))
+
+# Printing the entered number
+print(f"The number is {number}")
+
+# Checking if the number is odd or even and printing the result
+if number % 2 == 1:
+    print(False)
+else:
+    print(True)
